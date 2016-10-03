3c2508eea7f2f448148bd324a1f70bc0f5808ee1
rainbow82 <shannonbeach1982@gmail.com>
2016-10-03 18:44:02 -0400

    got it up and running

---
+++ mvc crud/mvc crud/Models/stuff.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace mvc_crud.Models
+{
+    
+        public class Planet
+        {
+            public int PlanetId { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class PlanetContext : DbContext
+        {
+            public DbSet<Planet> Planets { get; set; }
+        }
+
+        
+
+    
+}
---
+++ mvc crud/mvc crud/PlanetsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using mvc_crud.Models;
+
+namespace mvc_crud
+{
+    public class PlanetsController : Controller
+    {
+        private PlanetContext db = new PlanetContext();
+
+        // GET: Planets
+        public ActionResult Index()
+        {
+            return View(db.Planets.ToList());
+        }
+
+        // GET: Planets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Planet planet = db.Planets.Find(id);
+            if (planet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(planet);
+        }
+
+        // GET: Planets/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Planets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PlanetId,Name")] Planet planet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Planets.Add(planet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(planet);
+        }
+
+        // GET: Planets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Planet planet = db.Planets.Find(id);
+            if (planet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(planet);
+        }
+
+        // POST: Planets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PlanetId,Name")] Planet planet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(planet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(planet);
+        }
+
+        // GET: Planets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Planet planet = db.Planets.Find(id);
+            if (planet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(planet);
+        }
+
+        // POST: Planets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Planet planet = db.Planets.Find(id);
+            db.Planets.Remove(planet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
