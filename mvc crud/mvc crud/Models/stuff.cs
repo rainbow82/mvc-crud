d1c8a20301ff16ff41bfd11708f49e27a3acd110
rainbow82 <shannonbeach1982@gmail.com>
2016-10-03 19:20:26 -0400

    added population

--- mvc crud/mvc crud/Models/stuff.cs
+++ mvc crud/mvc crud/Models/stuff.cs
@@ -11,11 +11,32 @@ namespace mvc_crud.Models
         {
             public int PlanetId { get; set; }
             public string Name { get; set; }
+            public long Population { get; set; }
         }
 
         public class PlanetContext : DbContext
         {
             public DbSet<Planet> Planets { get; set; }
+
+            public PlanetContext()
+            {
+                Database.SetInitializer<PlanetContext>(
+                    new MyInitializer()
+                    );
+            }
+
+        public class MyInitializer : DropCreateDatabaseIfModelChanges<PlanetContext>
+        {
+            protected override void Seed(PlanetContext context)
+            {
+                base.Seed(context);
+                context.Planets.Add(new Planet()
+                {
+                    Name = "earth",
+                    Population = 700000000
+                });
+                context.SaveChanges();
+            }
         }
 
         
--- mvc crud/mvc crud/PlanetsController.cs
+++ mvc crud/mvc crud/PlanetsController.cs
@@ -46,7 +46,7 @@ namespace mvc_crud
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "PlanetId,Name")] Planet planet)
+        public ActionResult Create([Bind(Include = "PlanetId,Name,Population")] Planet planet)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace mvc_crud
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "PlanetId,Name")] Planet planet)
+        public ActionResult Edit([Bind(Include = "PlanetId,Name,Population")] Planet planet)
         {
             if (ModelState.IsValid)
             {
